8b0a1b9b1bc544f2c7a170e001b01511bef8d227
TomW <tom.whiston@gmail.com>
2024-10-22 22:21:16 +0100

    refactor: rename value_sync to value_trigger

--- operators/value_trigger.js
+++ operators/value_trigger.js
@@ -9,9 +9,9 @@
 
 @meta({
 	dsponly : true,
-	digest : "sync a value to a trigger"
+	digest : "trigger an output of value on input trigger"
 })
-class value_sync extends StatefulOperator
+class value_trigger extends StatefulOperator
 {
 
 	@option({ digest : "Initial value", comments : "The initial value from which to begin counting. Note that this only applies to the first count loop and only when counting is enabled at startup (reset 0), because resetting the counter~ during run time will always start counting from zero." }) init = 0;
@@ -19,7 +19,7 @@ class value_sync extends StatefulOperator
 	triggered : bool = false;
 
 	@meta({
-		a : { digest : "value to sync", comments : "" },
+		a : { digest : "value to trigger", comments : "" },
 		b : { digest : "trigger"},
 		threshold : { digest : "set the threshold at which a new trigger will be registered", defaultarg: 1, comments : "The threshold at which the gate/trigger will be registered, useful for when your signal does not hit 1." },
 		out1 : { digest : "value", comments : "triggered value or 0" },
